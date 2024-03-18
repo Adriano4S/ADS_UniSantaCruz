0ffa218c0abe6a3297b2d67c140c312a4d53431d
Cleber soares <arthur.cleber@hotmail.com>
2024-03-18 19:49:40 -0300

    Update ProjetoAdsApplication.java

--- CRUD_em_Java/src/main/java/com/pacote_teste/projeto_ads/ProjetoAdsApplication.java
+++ CRUD_em_Java/src/main/java/com/pacote_teste/projeto_ads/ProjetoAdsApplication.java
@@ -5,9 +5,55 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoAdsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoAdsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoAdsApplication.class, args);
+    }
 }
+
+class Task {
+    private String titulo;
+    private String descricao;
+    private String status;
+
+    public Task(String titulo, String descricao, String status) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
+
+
+
